a492e46efd0477d2ef92ac6ab3d39bfa8184d52b
Erlend Dancke Sandorf <edsandorf@gmail.com>
2025-02-28 08:57:30 +0100

    Added package startup message and information

---
+++ R/zzz.R
@@ -0,0 +1,41 @@
+#' Print package startup message
+#' 
+#' The function is called when the package is loaded through the `library` or
+#' `require` functions. It prints a message to the console.
+#' 
+#' @param libname The library name
+#' @param pkgname The package name
+#' 
+#' @return Nothing
+.onAttach <- function(libname, pkgname) {
+  installed_version <- utils::packageDescription("cmtools", fields = "Version")
+  
+  description <- tryCatch({
+    readLines("https://raw.githubusercontent.com/edsandorf/cmtools/master/DESCRIPTION")
+    
+  }, warning = function(w) {
+    return("NA")
+    
+  }, error = function(e) {
+    return("NA")
+    
+  })
+  
+  if (length(description) == 1) {
+    remote_version <- description
+    
+  } else {
+    remote_version <- gsub("Version:\\s*", "", description[grep('Version:', description)])    
+    
+  }
+  
+  packageStartupMessage(
+    "You are currently using cmtools version: ",
+    installed_version, "\n\n",
+    "The latest version is: ", remote_version, "\n\n",
+    "To access the latest version, please run \n",
+    "devtools::install_github('edsandorf/cmtools') \n\n",
+    "To cite this package: \n",
+    "utils::citation('cmtools')"
+  )
+}
