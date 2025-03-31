21b14ae5ed1938503a1ab4c90458cf99929df697
Erlend Dancke Sandorf <edsandorf@gmail.com>
2025-03-31 14:40:24 +0200

    Added simple assertion tests

---
+++ tests/testthat/test-assertions.R
@@ -0,0 +1,14 @@
+test_that("is_bgw() correctly returns TRUE and FALSE", {
+  expect_true(is_bgw(model))
+  expect_false(is_bgw(list(x = runif(10))))
+})
+
+test_that("has_scores() correctly returns TRUE and FALSE", {
+  expect_true(has_scores(modified_model))
+  expect_false(has_scores(model))
+  expect_error(has_scores(list(x = runif(10))))
+})
+
+test_that("converged() correctly returns TRUE and FALSE", {
+  expect_true(converged(model))
+})
