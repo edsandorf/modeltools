da0c2025aa4c5093a3b7e52593672b79ae7dd33b
Erlend Dancke Sandorf <edsandorf@gmail.com>
2025-03-31 14:37:06 +0200

    Updated assertions: 1) Added additional convergence messages to converged() 2) Added has_scores() to test if the model object contains the scores matrix. Useful for calculating robust ses. 3) Added is_bgw() to test whether an object is of class bgw_mle() useful for controlling flow.

--- R/assertions.R
+++ R/assertions.R
@@ -4,10 +4,12 @@
 #' the optimization converged to a local optimum.
 #' 
 #' Objects of class bgw_mle will have the following codes:
+#' 0 - Initial f(x) cannot be computed
 #' 4 - Relative function convergence 
 #' 5 - X- and relative function convergence
 #' 7 - Singular convergence
 #' 8 - False convergence
+#' 9 - Function evaluation limit
 #' 
 #' @param x A model object
 #' @param ... Additional arguments
@@ -26,3 +28,45 @@ converged <- function(x, ...) {
     boolean
   )
 }
+
+#' Check if the model object is of class `bwg_mle`
+#' 
+#' @param x A model object of class `bgw_mle`
+#' 
+#' @return A boolean value equal to TRUE if the object is of class `bgw_mle`.
+#' 
+#' @export
+is_bgw <- function(x) {
+  return(
+    class(x) == "bgw_mle"
+  )
+}
+
+#' Check if the model object contains the scores
+#' 
+#' The scores matrix contains the gradient observations and are used to calcualte
+#' the robust variance-covariance matrix. The function is only implemented and 
+#' used for objects of class `bgw_mle`. If called on other objects, the function
+#' will throw an error.
+#' 
+#' @inheritParams is_bgw
+#' 
+#' @return A boolean value equal to TRUE if the object contains the scores.
+#' 
+#' @export
+has_scores <- function(x) {
+  if (!is_bgw(x)) {
+    cli_abort(
+      c(
+        "{.var x} must be of class `bgw_mle`",
+        "x" = "You've supplied an object of {.cls {class(x)}}."
+      )
+    )
+  }
+  
+  return(
+    "scores" %in% names(x)
+  )
+}
+
+
